ec2074494bb75999f06828e354b282f9a8eb1cd9
Alex Hurley <ahh1539@rit.edu>
2021-02-26 19:19:21 -0500

    initial commit

---
+++ AlexValheimTest/TestMod.cs
@@ -0,0 +1,45 @@
+﻿using HarmonyLib;
+using System.Reflection;
+using System;
+using UnityEngine;
+using System.IO;
+using System.Threading;
+
+
+
+
+namespace TestMod
+{
+    public class TestMod
+    {
+        public static void Main(string[] args)
+        {
+            new Thread(() =>
+            {
+                Harmony harmony = new Harmony("com.valheim.alex_test");
+
+                while (AccessTools.Method(typeof(Player), "GetMaxCarryWeight") == null)
+                {
+                    Thread.Sleep(100);
+
+                }
+                harmony.PatchAll();
+            }).Start();
+        }
+    }
+
+    [HarmonyPatch]
+    public static class Patches
+    {
+        private static MethodInfo isPlayerCrouching = AccessTools.Method(typeof(Player), "IsCrouching");
+        private static MethodInfo setPlayerCrouching = AccessTools.Method(typeof(Player), "SetCrouch");
+
+
+        [HarmonyPrefix]
+        [HarmonyPatch(typeof(Player), "Update")]
+        public static void TestMyMod(Player __instance)
+        {
+            Console.instance.Print("test 1 ");
+        }
+    }
+}
