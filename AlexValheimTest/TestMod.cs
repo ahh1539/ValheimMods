f14cc72ce36ed0a1936e79bfd9413147bb861501
Alex Hurley <ahh1539@rit.edu>
2021-02-27 17:50:18 -0500

    work in progress

--- AlexValheimTest/TestMod.cs
+++ AlexValheimTest/TestMod.cs
@@ -31,15 +31,41 @@ namespace TestMod
     [HarmonyPatch]
     public static class Patches
     {
-        private static MethodInfo isPlayerCrouching = AccessTools.Method(typeof(Player), "IsCrouching");
-        private static MethodInfo setPlayerCrouching = AccessTools.Method(typeof(Player), "SetCrouch");
-
-
         [HarmonyPrefix]
         [HarmonyPatch(typeof(Player), "Update")]
-        public static void TestMyMod(Player __instance)
+        public static void TestModWorks(Player __instance)
         {
-            Console.instance.Print("test 1 ");
+            try
+            {
+                MethodInfo isPlayerCrouching = AccessTools.Method(typeof(Player), "IsCrouching");
+                MethodInfo getCarryWeight = AccessTools.Method(typeof(Player), "GetMaxCarryWeight");
+                Console.instance.Print("previous carry weight " + getCarryWeight.Invoke(__instance, null) + " iscrouching: " + isPlayerCrouching.Invoke(__instance, null));
+            }
+            catch (Exception e)
+            {
+                Console.instance.Print("ERROR: " + e);
+            }
         }
+
+        //[HarmonyPrefix]
+        //[HarmonyPatch(typeof(Player), "Update")]
+        //public static void TestSetStam(Player __instance)
+        //{
+        //    try
+        //    {
+        //        MethodInfo GetMaxStamina = AccessTools.Method(typeof(Player), "GetMaxStamina");
+        //        MethodInfo SetMaxStamina = AccessTools.Method(typeof(Player), "SetMaxStamina");
+        //        if ((float)GetMaxStamina.Invoke(__instance, null) < 100.0)
+        //        {
+        //            Console.instance.Print("previous max stam " + GetMaxStamina.Invoke(__instance, null));
+        //            SetMaxStamina.Invoke(__instance, new object[] { 101.00 , false });
+        //            Console.instance.Print("new max stam " + GetMaxStamina.Invoke(__instance, null));
+        //        }
+        //    }
+        //    catch (Exception e)
+        //    {
+        //        Console.instance.Print("ERROR: " + e);
+        //    }
+        //}
     }
 }
